c8f48e3550d8964c42d72c75aeecd911ca521523
jace-mathieu <jacemathieu96@gmail.com>
2022-06-18 14:36:58 -0400

    create custom getCurrentUser decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
-import { Request } from "express";
+import { GetCurrentUser } from "src/common/decorators";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 import { Tokens } from "./types";
@@ -24,16 +24,16 @@ export class AuthController {
     @UseGuards(AuthGuard("jwt-access")) // use jwt-access strategy as the auth guard
     @Post("/signout")
     @HttpCode(HttpStatus.OK) // return 200 if truthy
-    signout(@Req() req: Request) {
-        const user = req.user; // retrieve user from request object in the strategy
-        return this.authService.signout(user["sub"]);
+    signout(@GetCurrentUser("sub") userId: number) {
+        // retrieve user from request object in the strategy
+        return this.authService.signout(userId);
     }
 
     @UseGuards(AuthGuard("jwt-refresh")) // use jwt-refresh strategy as the auth guard
     @Post("/refresh")
     @HttpCode(HttpStatus.OK) // return 200 if truthy
-    refreshTokens(@Req() req: Request): Promise<Tokens> {
-        const user = req.user;
-        return this.authService.refreshTokens(user["sub"], user["refreshToken"]);
+    refreshTokens(@GetCurrentUser("sub") userId: number, @GetCurrentUser("refreshToken") rt: string): Promise<Tokens> {
+        // retrieve user from request object in the strategy
+        return this.authService.refreshTokens(userId, rt);
     }
 }
